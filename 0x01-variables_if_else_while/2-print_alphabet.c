ea421c40b88d316f79c61e6802021e69f3ad57f0
Afaf23 <bouzirafef@gmail.com>
2023-09-08 12:49:53 -0700

    alphab

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-	char alph;
-	for (alph = 'a'; alph <= 'z'; alph++)
-		{
-			putchar(alph);
-		}
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
 	return (0);
+
 }
