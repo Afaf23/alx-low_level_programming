96c6b03e2db3a7217c471ec05e21ce05cb129854
Afaf23 <bouzirafef@gmail.com>
2023-09-08 12:24:50 -0700

    lettres

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			putchar(alph);
+		}
+	return (0);
+}
