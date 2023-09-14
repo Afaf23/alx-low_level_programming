1a9a194f548e8f13ab538a3958b226d9a6860808
Afaf23 <bouzirafef@gmail.com>
2023-09-14 09:19:13 -0700

    task 3 print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char n;
-	
-	for (n = '0'; n < 10; n++)
-	{
-		_putchar(n);
-	}
-	_putchar('\n');
+        char n;
+
+        for (n = '0'; n < 10; n++)
+        {
+                _putchar(n);
+        }
+        _putchar('\n');
 }
