ead22963e9ad6937dccdfff38e49972cd5347cc6
Afaf23 <bouzirafef@gmail.com>
2023-09-14 11:51:36 -0700

    last adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,21 +3,21 @@
 /**
  * print_number - Prints an integer
  * @n: The integer to be printed
- *
  * Return: void
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
+		n *= -1;
+		k = n;
 		putchar('-');
-		num = -num;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if (k != 0)
+		print_number(k);
 
-	putchar((num % 10) + '0');
+	putchar((unsigned int) n % 10  + '0');
 }
