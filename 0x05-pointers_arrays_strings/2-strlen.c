b8492d14080c352b8ed3a0691783883cdcc226ae
Afaf23 <bouzirafef@gmail.com>
2023-09-18 09:32:12 -0700

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+	{
+		int i;
+
+		for (i = 0; *s != '\0'; i++)
+		{
+			i++;
+		}
+		return (i);
+	}
