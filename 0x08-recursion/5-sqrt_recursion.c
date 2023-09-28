c95d214c2d4478515a797d5e08dc3ae28d9d1f8e
Afaf23 <bouzirafef@gmail.com>
2023-09-28 13:18:01 -0700

    0x08

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - calculates the natural square root of n.
+ * @n: base number.
+ *
+ * Return: the square root of n.
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	if (i * i == n)
+		return (i);
+	else
+		return (-1);
