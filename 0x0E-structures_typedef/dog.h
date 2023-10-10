ab9ac6599ed52d67ca78e0bd59d33342ed27f769
Afaf23 <bouzirafef@gmail.com>
2023-10-10 12:24:14 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h
+#define _dog_h
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @owner: owner's name
+ * @age: dog's age
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* _dog_h */
