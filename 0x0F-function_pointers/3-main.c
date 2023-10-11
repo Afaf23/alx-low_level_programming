a05e9510a189e3664385d9b7bc8794a5c52d33f8
Afaf23 <bouzirafef@gmail.com>
2023-10-11 12:00:03 -0700

    ptr to func

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * @argc: the number of the parameters
@@ -6,28 +8,23 @@
 * Return: 0 in success
 */
 int main(int argc, char *argv[])
-{
-	int n1, n2, result;
-	int (*p)(int, int);
+	{
+	int (*oprt)(int, int);
 
-	if (argc < 4 || argc > 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
+	oprt = get_op_func(argv[2]);
 
-	p = get_op_func(argv[2]);
-
-	if (p == NULL)
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = p(n1, n2);
 
-	printf("%d\n", result);
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
-}
+	}	
