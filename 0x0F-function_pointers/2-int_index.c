0e29a7b4dcc68ab63bce2c823ff290953567bc50
Afaf23 <bouzirafef@gmail.com>
2023-10-11 11:39:12 -0700

    ptr to func

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * int_index - this functions find the index of a int
  *@array: the array to find the index
@@ -10,17 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-	{
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
-	}
-
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i], 0) != 0)
-	{
+		if (cmp(*(array + i)))
 			return (i);
 	}
-	}
+
 	return (-1);
 }
