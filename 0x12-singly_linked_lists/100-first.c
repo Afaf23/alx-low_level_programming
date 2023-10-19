28afb1c8196d9be5ad8284f6d890e6024f19b4f9
Afaf23 <bouzirafef@gmail.com>
2023-10-19 11:58:55 -0700

    lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * first - funcion that prints a specific string before the main
+ * Return: void.
+ */
+void first(void)
+	{
+		printf("You're beat! and yet, you must allow,\n");
+		printf("I bore my house upon my back!\n");
+	}
